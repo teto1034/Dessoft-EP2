9ec24c0aa95155da26c761bf754d82805b36507c
teto1034 <140311484+teto1034@users.noreply.github.com>
2025-10-06 13:29:09 -0300

    Exercício 1 do Prairielearn - função define_posicoes

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+#Exercício 1 do Prairielearn
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    listafinal = []
+    if orientacao == 'vertical':
+        for i in range(tamanho):
+            listafinal.append([linha + i, coluna])
+    elif orientacao == 'horizontal':
+        for i in range(tamanho):
+            listafinal.append([linha, coluna + i])
+    return listafinal
