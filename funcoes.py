dc3062c4826e77d8e8c1ede3d7096be8b2ddfcbe
carlosecs4 <carlosecs4@al.insper.edu.br>
2025-10-07 21:21:20 -0300

    Exercício 3

--- funcoes.py
+++ funcoes.py
@@ -17,3 +17,11 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
     else:
         frota[nome_navio].append(navio_posicao)
     return frota
+
+#Exercício 3 do Prairielearn
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 0:
+        tabuleiro[linha][coluna] = '-'
+    elif tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
+    return tabuleiro
